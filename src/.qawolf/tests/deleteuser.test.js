6095dbaa2047adc2ec1d2efedfa692f6d630dafe
Hung Tran <s3715272@rmit.edu.vn>
2020-04-17 23:08:19 +1000

    edited e2e

--- src/.qawolf/tests/deleteuser.test.js
+++ src/.qawolf/tests/deleteuser.test.js
@@ -26,37 +26,37 @@ describe('deleteuser', () => {
     }
   });
 
-  browser = await page.goto('http://localhost:3000/')
+  await page.goto('http://localhost:3000/')
   
   it('can click "username" input', async () => {
-    await browser.click(selectors[0]);
+    await page.click(selectors[0]);
   });
   
   it('can clear "username" input', async () => {
-    await browser.type(selectors[1], "TestUser");
+    await page.type(selectors[1], "TestUser");
   });
   
   it("can click input", async () => {
-    await browser.click(selectors[2]);
+    await page.click(selectors[2]);
   });
   
   it('can click "title" input', async () => {
-    await browser.click(selectors[3]);
+    await page.click(selectors[3]);
   });
   
   it('can clear "title" input', async () => {
-    await browser.type(selectors[4], "Task 1");
+    await page.type(selectors[4], "Task 1");
   });
   
   it("can click input", async () => {
-    await browser.click(selectors[5]);
-    const hasText = await browser.hasText("Task 1");
+    await page.click(selectors[5]);
+    const hasText = await page.hasText("Task 1");
     expect(hasText).toBe(true);
   });
   
   it('can click "delete" link', async () => {
-    await browser.click(selectors[6]);
-    const hasText = await browser.hasText("Task 1");
+    await page.click(selectors[6]);
+    const hasText = await page.hasText("Task 1");
     expect(hasText).toBe(false);
   });
 });
